b99ccb85a88cd9839ac4841d22516f64a5037e70
Kimiya Kitani <kimipooh@gmail.com>
2018-06-15 14:19:43 +0900

    added wp-installer

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # wp-cli-handson-tools
 WP-CLI ハンズオン用のツール類をまとめたものです。
+Linux, macOS を想定しています。
+
+## mamp-php-path.txt
+MAMP上の WordPress を WP-CLI で制御するために必要な、MAMPのPHPへのパスを探して設定してくれる .bash_profile 用の設定が入っています。MAMPをつかう場合には、この中身を $HOME/.bash_profile の末尾に追加しておいてください。
+
+## wp-installer.sh
+MAMPをコマンドラインから一発インストールするためのスクリプトです。
+
+このファイルを開いて、下記の設定を変更してください。
+以下のサンプル例は、MAMPで http://localhost:8888/demo としてアクセスできる WordPress のインストール設定です
+
+export sitename="demo"
+export dbuser="root"
+export dbpass="root"
+export dbname="demo"
+export dbprefix="wp_"
+export dbhost="127.0.0.1:8889"
+export wp_title="Demo Site"
+export site_url="http://localhost:8888/demo"
+export admin_user="admin"
+export admin_password="admin"
+export admin_email="admin@example.com"
+export wp_install_dir="/Applications/MAMP/htdocs/"
+
+利用方法は、
+sh  wp-installer.sh
+で利用できます。wp_install_dir で指定したフォルダのなかに sitename で指定したフォルダがあれば、上書き防止のため処理を終了してインストールしません。予めインストール先に指定したフォルダがないことを確認してください。
--- wp-installer.sh
+++ wp-installer.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
+## 環境設定（それぞれの環境に合わせて変更してください）
+## 以下のサンプル例は、MAMPで http://localhost:8888/demo としてアクセスできる WordPress のインストール設定です
 export sitename="demo"
+export dbuser="root"
+export dbpass="root"
+export dbname="demo"
+export dbprefix="wp_"
+export dbhost="127.0.0.1:8889"
+export wp_title="Demo Site"
+export site_url="http://localhost:8888/demo"
+export admin_user="admin"
+export admin_password="admin"
+export admin_email="admin@example.com"
+export wp_install_dir="/Applications/MAMP/htdocs/"
+
+# WordPress インストール先フォルダが存在したら、処理を停止する（上書き怖い！）
+if [ -d "${wp_install_dir}/${sitename}" ]; then
+  echo "Folder exists. Stopped WordPress Installer on WP-CLI."
+  exit
+fi
+
+mkdir -p ${wp_install_dir}/${sitename}
+
+cd ${wp_install_dir}/${sitename}
 
 wp core download --force --locale=ja
 
-wp core config --dbname=$sitename --dbuser=root --dbpass=root --dbhost=localhost:8889
+wp core config --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" --dbhost="$dbhost" --dbprefix="$dbprefix"
 
 wp db create
 
-wp core install --url=http://localhost:8888/$sitename --title=Demo --admin_user=admin  --admin_password=admin --admin_email=admin@example.com
+wp core install --url="$site_url" --title="$wp_title" --admin_user="$admin_user"  --admin_password="$admin_password" --admin_email="$admin_email"
 
 wp plugin activate wp-multibyte-patch
 
