bcb97b1f9c2ff9bc6757c136a555b155fbdfb430
Kimiya Kitani <kimipooh@gmail.com>
2018-06-22 23:12:11 +0900

    fixed wp-update.bat

--- wp-update.bat
+++ wp-update.bat
@@ -5,21 +5,10 @@ if "%1" == "" (
   goto end
 )
 
-wp cli update --yes
-
-# バックアップ
-wp @%1  backwpup  start  1
-
-# 本体更新
-wp @%1 core update  --force  --locale=ja 
-
-# プラグイン更新
-wp @%1 plugin update --all
-
-# テーマ更新
-wp @%1 theme update --all
-
-# 言語更新
+wp @%1  backwpup  start  1 && ^
+wp @%1 core update  --force  --locale=ja && ^
+wp @%1 plugin update --all && ^
+wp @%1 theme update --all && ^
 wp @%1 language core update
 
 :end
